a9ee5e4e87090ca11cab3c43e11fcb7376c25512
江绍坤 <1455935316@qq.com>
2019-11-22 17:04:48 +0800

    feat(all): 去掉分号，事件单元测试

--- src/components/UIButton.test.tsx
+++ src/components/UIButton.test.tsx
@@ -1,6 +1,8 @@
 import UIButton from './UIButton'
 import React from 'react'
-import TestRenderer from 'react-test-renderer';
+import ReactDOM from 'react-dom'
+import TestRenderer from 'react-test-renderer'
+import {act, Simulate} from 'react-dom/test-utils'
 
 // 分支测试
 it('传了type，类名应该包含btn-type', () => {
@@ -20,7 +22,7 @@ it('没传type，类名应该包含btn-type', () => {
   const str = testRenderer.toJSON()
   const className = str && str.props.className
   expect(className).toBe('btn')
-});
+})
 
 // 参数测试
 it('按钮文案与参数相等', () => {
@@ -33,4 +35,22 @@ it('按钮文案与参数相等', () => {
   const children = str && str.children
   const result = children && children.join('')
   expect(result).toBe(text)
-});
+})
+
+// 事件测试
+it('回调正常点击', () => {
+  const handleClick = jest.fn()
+  const component = <UIButton text={'A button'} handleClick={handleClick} />
+  const container = document.createElement('div')
+  act(() => {
+    ReactDOM.render(component, container)
+  })
+
+  const button = container.querySelector('button')
+
+  // 测试 render 和 componentDidUpdate
+  act(() => {
+    button && Simulate.click(button)
+  })
+  expect(handleClick).toHaveBeenCalled()
+})
